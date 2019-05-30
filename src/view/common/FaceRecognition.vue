82aa4a21c3cf0c06b9863420ed7abbc61e44198a
francisw1989 <francis@francisdeMacBook-Air.local>
2019-05-30 14:34:21 +0800

    32

--- src/view/common/FaceRecognition.vue
+++ src/view/common/FaceRecognition.vue
@@ -4,7 +4,7 @@
             <div class="cont relative">
                 <!-- <div class="m"></div> -->
             </div>
-            <p class="center top40 p">识别中……</p>
+            <p class="center top20 p">识别中……</p>
         </div>
         <p class="colblue font24b top40 center">请将头像置于识别框中</p>
     </div>
